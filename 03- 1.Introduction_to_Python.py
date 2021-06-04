773b89a49f297627d97c8db69f504c2dbd7b622b
Lakshmi B <82225535+Lakshmishiny77@users.noreply.github.com>
2021-06-04 15:56:57 +0530

    Add files via upload
    Let's start learning by actual python coding

---
+++ 03- 1.Introduction_to_Python.py
@@ -0,0 +1,15 @@
+# Im running the code by installing Python (version - 3.9.4)
+##Link - "https://www.python.org/downloads/"
+
+#this file is running on IDLE shell
+
+a = "1. INTRODUCTION TO PYTHON"
+b = "\nPlease check PPT3 for the information"
+
+c = a+b
+
+print(c)
+print("\nThank you")
+
+print("learning is the never ending process \U0001f600")
+
