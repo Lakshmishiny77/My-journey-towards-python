6dfe411ff49628f6dce904e74125abdbfa9d5a1c
Lakshmi B <82225535+Lakshmishiny77@users.noreply.github.com>
2021-06-07 14:22:59 +0530

    Add files via upload
    Please uncomment and check few examples of set in python

---
+++ 09- 2.1.3.Scenarios_of_Set_in_Data_Types.py
@@ -0,0 +1,63 @@
+##2.1.1 SCENARIOS OF SET
+
+#printing set
+s = {"apple","dell","hp"}
+print(s)
+
+##1.access item in sets
+#example1:
+##s = {"apple","dell","hp"}
+##for x in s:
+##    print(x)
+#example2:
+##s = {"apple","dell","hp"}
+##print("poco" in s)
+
+##2.change items in sets  - once a set is created, you cannot change its items,but you can add new items.
+
+#add items in sets
+#example1:
+##s = {"apple","dell","hp"}
+##s.add("Mi")
+##print(s)
+#example2:
+##s = {"apple","dell","hp"}
+##s.update(["mi","poco"])
+##print(s)
+
+#3.remove items in sets
+#example1:
+##s = {"apple","dell","hp","poco"}
+##s.remove("poco")
+##print(s)
+#example2:
+##s = {"apple","dell","hp","poco"}
+##s.pop()
+##print(s)
+#example3:
+##s = {"apple","dell","hp","poco"}
+##s.discard("dell")
+##print(s)
+#example4:
+##s = {"apple","dell","hp","poco"}
+##s.clear()
+##print(s)
+
+##4.join 2 sets
+
+##s1 = {"apple","dell","hp"}
+##s2 = {"mi","oppo"}
+##s3 = s1.union(s2)
+##print(s3)
+
+##5.set constructor
+
+##s = set(("apple","microsoft","google"))
+##print(s)
+
+print("\nThank you")
+
+print("\t\t-learning is the never ending process \U0001f600")
+
+
+
